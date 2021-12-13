daba3e24dbfef2974abc9b1e18aa9236a85c1c3d
Maximiliano Pisso <maximiliano.pisso@gmail.com>
2021-12-13 19:15:38 -0300

    subo api

---
+++ Desafio4Movies/src/main/java/com/example/desafioMovies/Desfio4MoviesApplication.java
@@ -0,0 +1,14 @@
+package com.example.desafioMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Desfio4MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desfio4MoviesApplication.class, args);
+	}
+
+}
---
+++ Desafio4Movies/src/test/java/com/example/desafioMovies/Desfio4MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafioMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desfio4MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
